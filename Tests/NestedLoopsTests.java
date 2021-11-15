6c89529dec06ce437b1810f7e970e6ae7f9e07b7
sseob2 <leesa@ssfs.org>
2021-11-15 08:12:21 -0500

    nested loops

--- Tests/NestedLoopsTests.java
+++ Tests/NestedLoopsTests.java
@@ -21,8 +21,8 @@ public class NestedLoopsTests {
 
     @Test
     void fizzBuzzTest() {
-        String expected = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz\n" +
-                "Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz\n" +
+        String expected = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz \n" +
+                "Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz \n" +
                 "41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz ";
         String actual = NestedLoops.fizzBuzz(50);
         assertEquals(expected, actual);
--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,17 +1,43 @@
 public class NestedLoops {
 
     public static String xSquare(int n) {
-
-        return "";
+        String squareX = "";
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++){
+                squareX += "X";
+            }
+            squareX += "\n";
+        }
+        return squareX;
     }
 
     public static String flippedTriangle(int n) {
-
-        return "";
+        String triangle = "";
+        for(int i = 0; i <= n; i++){
+            for(int j = 6; j >= i; j--){
+                triangle += "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
     }
 
     public static String fizzBuzz(int n) {
-
-        return "";
+        String fizzBuzzReturn = "";
+        for(int i = 1; i <= n; i++){
+            if (i % 15 == 0){
+                fizzBuzzReturn += "FizzBuzz ";
+            } else if (i % 3 == 0 && i % 15 != 0){
+                fizzBuzzReturn += "Fizz ";
+            } else if (i % 5 == 0 && i % 15 != 0){
+                fizzBuzzReturn += "Buzz ";
+            } else {
+                fizzBuzzReturn += i + " ";
+            }
+            if(i % 20 == 0){
+                fizzBuzzReturn += "\n";
+            }
+        }
+        return fizzBuzzReturn;
     }
 }
